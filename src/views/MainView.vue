cbf3a7b5d5f9e2da1aed6c8e0838b13ea59e083c
sopand <pows1031@naver.com>
2024-01-10 17:11:13 +0900

    axios Get

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -20,8 +20,8 @@
 		<div class="album py-5 bg-body-tertiary">
 			<div class="container">
 				<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-					<div class="col" v-for="i in 12" :key="i">
-						<CardBar />
+					<div class="col" v-for="(item, idx) in state.items" :key="idx">
+						{{ item }}
 					</div>
 				</div>
 			</div>
@@ -30,11 +30,25 @@
 </template>
 
 <script>
-import CardBar from '@/components/Card.vue';
+import axios from 'axios';
+import { reactive } from 'vue';
 export default {
 	name: 'main-view',
-	components: {
-		CardBar,
+	components: {},
+	setup() {
+		const state = reactive({
+			items: [],
+		});
+
+		axios.get('/api/user/').then(({ data }) => {
+			console.log(data);
+			state.items = data;
+			console.log(state.items);
+		});
+
+		return {
+			state,
+		};
 	},
 };
 </script>
